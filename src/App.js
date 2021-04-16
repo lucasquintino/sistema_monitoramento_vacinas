3576d4360fc66cf82f4a73e0d3c29b6b2ece98b5
Lucas Quintino <lucasqf_2@hotmail.com>
2021-04-16 14:18:17 -0300

    again

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 
 import Routes from "./routes";
 
 function App() {
   return (
-    <BrowserRouter>
+    <HashRouter>
       <Routes />
-    </BrowserRouter>
+    </HashRouter>
   );
 }
 
